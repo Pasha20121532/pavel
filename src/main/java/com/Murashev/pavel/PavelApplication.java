900e478be8f34bb215334dea1313ff0493e321f1
Pasha20121532 <Ipashrum@gmail.com>
2024-04-26 16:49:45 +0300

    Start

---
+++ src/main/java/com/Murashev/pavel/PavelApplication.java
@@ -0,0 +1,13 @@
+package com.Murashev.pavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PavelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Murashev/pavel/PavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Murashev.pavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
